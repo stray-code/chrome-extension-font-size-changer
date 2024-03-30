a345ea50bb2fe53b49008b48ebb6f7a43bc2b96f
stray-code <abc1007020@gmail.com>
2024-03-30 11:55:43 +0900

    manifest修正

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,26 @@
 import { defineConfig } from 'vite'
-import { crx } from '@crxjs/vite-plugin'
-import manifest from './manifest.json'
+import { crx, defineManifest } from '@crxjs/vite-plugin'
+
+const manifest = defineManifest({
+  manifest_version: 3,
+  name: 'フォントサイズ変更',
+  version: '1.0.0',
+  icons: {
+    16: 'assets/img/icon16.png',
+    48: 'assets/img/icon48.png',
+    128: 'assets/img/icon128.png'
+  },
+  action: {
+    default_icon: 'assets/img/icon16.png',
+    default_popup: 'src/popup/index.html',
+  },
+  content_scripts: [
+    {
+      js: ['src/content/main.ts'],
+      matches: ['http://*/*', 'https://*/*']
+    }
+  ],
+});
 
 export default defineConfig({
   plugins: [crx({ manifest })],
