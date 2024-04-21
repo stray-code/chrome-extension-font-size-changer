c01b85985c55bd4a07b9dba02b161b7349488389
stray-code <abc1007020@gmail.com>
2024-04-21 15:01:03 +0900

    メッセージに型を指定

--- src/background.ts
+++ src/background.ts
@@ -1,4 +1,6 @@
-chrome.runtime.onMessage.addListener((message) => {
+import { Message } from './types';
+
+chrome.runtime.onMessage.addListener((message: Message) => {
   if (message.type === 'SET_BADGE_TEXT') {
     chrome.action.setBadgeText({ text: message.text });
   }
